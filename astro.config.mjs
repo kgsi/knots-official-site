0985e6402292653f081c140ac91586f319833713
nao anzai <nao.anzai@caters.co.jp>
2025-11-12 20:41:31 +0900

    Revert "[ modify ] SSR再チャレンジ"
    This reverts commit 3bb13a439df623f0777722195d14984c8c8ebf0c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import partytown from '@astrojs/partytown'
-import cloudflare from '@astrojs/cloudflare'
+import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   adapter: cloudflare(),
-  image: {
-    service: { entrypoint: 'astro/services/noop' },
-  },
   integrations: [
     partytown({
       config: {
